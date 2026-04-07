178abae39b33b1c266926dbfd0b6f5c174d4d017
Nikita dange <nikitadange227@gmail.com>
2026-04-07 12:19:28 +0530

    Create standardpipeline.groovy

---
+++ vars/standardpipeline.groovy
@@ -0,0 +1,22 @@
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Build') {
+                steps {
+                    echo "Building Application..."
+                }
+            }
+            stage('Test') {
+                steps {
+                    echo "Running Unit Tests..."
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    echo "Deploying Application..."
+                }
+            }
+        }
+    }
+}
